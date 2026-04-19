a4d1c0b283ad10de58b9942f79169e06b5b56c84
felop <felopater.remon2020@gmail.com@gmail.com>
2026-04-19 04:01:09 +0200

    first commitkkkkkoo,

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,7 @@ import AdminDashboard from './AdminDashboard';
 import { getMembers } from '../actions/members';
 import { getProjects } from '../actions/projects';
 import { getSettings } from '../actions/settings';
-
+export const dynamic = "force-dynamic";
 export default async function AdminPage() {
   const cookieStore = await cookies();
   const isAdmin = cookieStore.get('admin_auth')?.value === 'true';
